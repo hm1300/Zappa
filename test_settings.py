465c5c97db725bb216d29ba3298f7e0649b72375
Muhammad Husein <justika.indonesia@gmail.com>
2018-09-25 21:43:07 +0700

    add sqs cappabilities

--- test_settings.py
+++ test_settings.py
@@ -19,7 +19,8 @@ AWS_EVENT_MAPPING = {
     'arn:aws:s3:1': 'test_settings.aws_s3_event',
     'arn:aws:sns:1': 'test_settings.aws_sns_event',
     'arn:aws:dynamodb:1': 'test_settings.aws_dynamodb_event',
-    'arn:aws:kinesis:1': 'test_settings.aws_kinesis_event'
+    'arn:aws:kinesis:1': 'test_settings.aws_kinesis_event',
+    'arn:aws:sqs:1': 'test_settings.aws_sqs_event'
 }
 
 ENVIRONMENT_VARIABLES={'testenv': 'envtest'}
@@ -56,6 +57,9 @@ def aws_kinesis_event(event, content):
 
 def authorizer_event(event, content):
     return "AUTHORIZER_EVENT"
+
+def aws_sqs_event(event, content):
+    return "AWS SQS EVENT"
 
 
 def command():
--- zappa/core.py
+++ zappa/core.py
@@ -2209,10 +2209,10 @@ class Zappa(object):
             http://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
         """
 
-        # The two stream sources - DynamoDB and Kinesis - are working differently than the other services (pull vs push)
+        # The two stream sources - DynamoDB, Kinesis, adn SQS - are working differently than the other services (pull vs push)
         # and do not require event permissions. They do require additional permissions on the Lambda roles though.
         # http://docs.aws.amazon.com/lambda/latest/dg/lambda-api-permissions-ref.html
-        pull_services = ['dynamodb', 'kinesis']
+        pull_services = ['dynamodb', 'kinesis', 'sqs']
 
         # XXX: Not available in Lambda yet.
         # We probably want to execute the latest code.
@@ -2457,7 +2457,10 @@ class Zappa(object):
                     function,
                     self.boto_session
                 )
-                print("Removed event " + name + " (" + str(event_source['events']) + ").")
+                print("Removed event {}{}.".format(
+                        name,
+                        " ({})".format(str(event_source['events'])) if 'events' in event_source else '')
+                )
 
     ###
     # Async / SNS
